22f563b0c92d575636f60936e54206fb0ee77f74
LegionDev370 <llegion630@gmail.com>
2023-11-20 21:53:22 +0500

    navbar added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      Navbar
+      <h2>Hello Navbar</h2>
+    </div>
+  );
 }
 
 export default Navbar;
