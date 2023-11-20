a4cd6cd52803758d76dea0aaed7233b506bae72c
LegionDev370 <llegion630@gmail.com>
2023-11-20 21:45:10 +0500

    header text pushed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
 
 function Header() {
-  return <div>Header</div>;
+  return (
+    <div>
+      Header
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        rerum nobis ipsum et nihil animi distinctio id, praesentium nostrum sint
+        reiciendis ducimus autem quo. Pariatur facilis quidem deserunt ipsum
+        sunt! Reprehenderit iusto nihil illum possimus officia, id aliquid
+        commodi quae molestias quasi sunt sint cumque sequi corrupti nostrum.
+        Aliquid officia et odio molestiae in reiciendis magni quam sunt vitae
+        eum.
+      </p>
+    </div>
+  );
 }
 
 export default Header;
